b6ce82545be8061c219f12667fcd5a4405f161c1
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2025-04-10 19:37:46 -0300

    hotfix: belt key name and next version

--- src/helper/belts.ts
+++ src/helper/belts.ts
@@ -1,55 +1,55 @@
 export const beltMap = {
   white: { label: 'Branca', color: '#ffffff', textColor: '#000000' },
   blue: { label: 'Azul', color: '#1e88e5', textColor: '#ffffff' },
-  blueGrade: { label: 'Azul-Grau', color: '#0d47a1', textColor: '#ffffff' },
+  bluegrade: { label: 'Azul-Grau', color: '#0d47a1', textColor: '#ffffff' },
   yellow: { label: 'Amarela', color: '#fdd835', textColor: '#000000' },
-  yellowGrade: {
+  yellowgrade: {
     label: 'Amarela-Grau',
     color: '#f9a825',
     textColor: '#000000',
   },
   orange: { label: 'Laranja', color: '#ff9800', textColor: '#000000' },
-  orangeGrade: {
+  orangegrade: {
     label: 'Laranja-Grau',
     color: '#e65100',
     textColor: '#ffffff',
   },
   green: { label: 'Verde', color: '#43a047', textColor: '#ffffff' },
-  greenGrade: { label: 'Verde-Grau', color: '#1b5e20', textColor: '#ffffff' },
+  greengrade: { label: 'Verde-Grau', color: '#1b5e20', textColor: '#ffffff' },
   brown: { label: 'Marrom', color: '#795548', textColor: '#ffffff' },
-  brownGrade: { label: 'Marrom-Grau', color: '#4e342e', textColor: '#ffffff' },
+  browngrade: { label: 'Marrom-Grau', color: '#4e342e', textColor: '#ffffff' },
   black: { label: 'Preta', color: '#000000', textColor: '#ffffff' },
-  black1Grade: {
+  black1grade: {
     label: 'Preta 1° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black2Grade: {
+  black2grade: {
     label: 'Preta 2° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black3Grade: {
+  black3grade: {
     label: 'Preta 3° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black4Grade: {
+  black4grade: {
     label: 'Preta 4° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black5Grade: {
+  black5grade: {
     label: 'Preta 5° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black6Grade: {
+  black6grade: {
     label: 'Preta 6° Grau',
     color: '#000000',
     textColor: '#ffffff',
   },
-  black7Grade: {
+  black7grade: {
     label: 'Preta 7° Grau',
     color: '#000000',
     textColor: '#ffffff',
